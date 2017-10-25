61ba202c0db9b879d30019772561e520dc7b8d04
Carmen Perezchica <crperezchica@gmail.com>
2017-10-24 17:19:14 -0700

    all five questions done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const username = prompt('But first what\s your name?');
 console.log('user said their name was:' + username);
 alert('Hi ' + username + ' Get ready to start playing. Have fun!')
 
-//prompt('Does Carmen like rainbows?');
+//Question 1: prompt('Does Carmen like rainbows?');
 const rainbows=prompt('Does Carmen like rainbows?');
 console.log('user said response to rainbows:' + rainbows);
 if (rainbows.toLowerCase() === 'yes'|| rainbows.toLowerCase()=== 'YES' || rainbows.toLowerCase()=== 'Y' || rainbows.toLowerCase() ==='y'){
@@ -17,23 +17,38 @@ if (rainbows.toLowerCase() === 'yes'|| rainbows.toLowerCase()=== 'YES' || rainbo
  
 
 
-//prompt('Is her natural hair color blue?');
+//Question 2: prompt('Is her natural hair color blue?');
 const haircolor=prompt('Is Carmen\s haircolor blue?');
 console.log('user said response to haircolor:' + haircolor);
 if (haircolor.toLowerCase () === 'yes' || haircolor.toLowerCase () === 'y'){
     alert('Sorry ' + username + ' Carmen\s haircolor is not blue right now');
 } else if (haircolor.toLowerCase () === 'no' || haircolor.toLowerCase () === 'n')
-    alert('You\re correct ' + username + ' Carmen\s hair is not blue');
+    alert('Correct ' + username + '. Carmen\s hair is not blue');
 
 
-//prompt('Does she like flowers?')
+//Questions 3: prompt('Does she like flowers?')
 
 const flowers=prompt('Does Carmen like flowers?');
 console.log('user said response to flowers was:' + flowers);
 if (flowers.toLowerCase () === 'yes' || flowers.toLowerCase () === 'y'){
-    alert('You got it ' + username + ' Carmen does like flowers');
+    alert('You got it! ' + username + ' Carmen does like flowers');
 } else if (flowers.toLowerCase () === 'no' || flowers.toLowerCase () === 'n')
     alert(username + ' Who doesn\t like flowers');
 
-//prompt('Does she own a white car?')
-//prompt('Is she a unicorn?')
+//Questions 4: prompt('Does she own a white car?')
+const whiteCar=prompt('Does Carmen own a white car?');
+console.log('user said response to color of car:' + whiteCar);
+if (whiteCar.toLowerCase () === 'yes' || whiteCar.toLowerCase () === 'y'){
+    alert('That\s right ' + username + ' Carmen does own a white car');
+} else if (whiteCar.toLowerCase () === 'no' || whiteCar.toLowerCase () === 'n')
+    alert('Sorry' + username + ' Carmen does drive a white car');
+
+//Question 5: prompt('Is she a unicorn?')
+const unicorn=prompt('Is Carmen a unicorn?');
+console.log('user response to unicorn question was:' + unicorn);
+if (unicorn.toLowerCase () === 'yes' || unicorn.toLowerCase () === 'y'){
+    alert('Oh ' + username + ' of course she is not a unicorn');
+} else if (unicorn.toLowerCase () === 'no' || unicorn.toLowerCase () === 'n')
+    alert(username + ' You are correct, she is not a unicorn');
+
+    alert(username + ' Thank you for playing! Have a nice day.')
